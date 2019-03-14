5fa053d353982d9b104a126e27f252536576d252
fallenfate1982 <anil@auratechtt.com>
2019-03-14 16:41:13 -0400

    Initial Files

---
+++ platforms/ios/www/js/settings.js
@@ -0,0 +1,9 @@
+var API_LOCATION = 'http://bcsl.auratechtt.com'; //bcslpayroll.auratechtt.com
+var API_USER = "atsl";
+var API_PASSWORD = "Password123#";
+var DEVICE_MODE = '1';
+var DEVICE_ID = localStorage.getItem("device_id");
+
+var IS_NOT_FIRST_RUN = localStorage.getItem("device_id");
+
+
