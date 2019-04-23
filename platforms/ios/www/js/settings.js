dbcd6334855fcdd06a5cb88f618e7aced4f9cd89
Anil Ramlochan <anilramlochan@gmail.com>
2019-04-23 19:46:21 -0400

    Removed Old Settings

--- platforms/ios/www/js/settings.js
+++ platforms/ios/www/js/settings.js
@@ -1,6 +1,6 @@
-var API_LOCATION = 'http://bcsl.auratechtt.com'; //bcslpayroll.auratechtt.com
-var API_USER = "atsl";
-var API_PASSWORD = "Password123#";
+var API_LOCATION = ''; 
+var API_USER = "";
+var API_PASSWORD = "";
 var DEVICE_MODE = '1';
 var DEVICE_ID = localStorage.getItem("device_id");
 
